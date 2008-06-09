133196b178d8277245a56e7aadc485ac3e4f944a
Toni Spets <toni.spets@gmail.com>
2008-06-09 21:22:58 +0300

    version bump to HM0.22.3, forgot 0.22.2 existed already

--- source/a_game.h
+++ source/a_game.h
@@ -85,9 +85,9 @@
 
 
 // AQ2:TNG Deathwatch - Updated the Version variables to show TNG Stuff
-#define ACTION_VERSION  "TNG 2.81 HM0.22.2"
+#define ACTION_VERSION  "TNG 2.81 HM0.22.3"
 #define TNG_VERSION		"AQ2: The Next Generation"
-#define TNG_VERSION2	"AQ2: The Next Generation 2.81 HM0.22.2"
+#define TNG_VERSION2	"AQ2: The Next Generation 2.81 HM0.22.3"
 // AQ2:TNG Deathwatch End
 //AQ2:TNG Slicer This is the max players writen on last killed target
 //SLIC2
